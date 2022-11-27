fd02286b908e43e86bbbdb5513a6a7785230e1ba
bolom <bolomozhnov@gmail.com>
2022-11-27 18:21:17 +0300

    Методы обьектов

--- src/Author.java
+++ src/Author.java
@@ -1,7 +1,7 @@
 public class Author {
     //public static int year;
-   private String name;
-   private String surName;
+    private String name;
+    private String surName;
 
     public Author(String name,String surName){
         this.name=name;
@@ -14,6 +14,35 @@ public class Author {
         return this.surName;
     }
 
+    public String printFullName(){
+        return "Имя "+ this.getName()+", Фамилия "+getSurName();
+    }
+    @Override
+    public boolean equals(Object other) {
+        if (this==other){
+            return true;
+        }
+        if(other==null||getClass()!=other.getClass()){
+            return false;
+        }
+        Author author=(Author) other;
+        if (getName()!=author.getName()){
+            return false;
+        }
+        if (getSurName()!=author.getSurName()){
+            return false;
+        }
+        return printFullName()!=null
+                ? printFullName().equals(author.printFullName())
+                : author.printFullName()==null;
 
+    }
+
+    @Override
+    public int hashCode(){
+        int result=name==null ?0: name.hashCode();
+        result=result+surName==null ?0:surName.hashCode();
+        return result;
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,47 @@ public class Main {
 
     public static void main(String[] args) {
         Author Tolstoy = new Author("Lev", "Tolstoy");
+        Author Tolstoy2 = new Author("Lev", "Tolstoy");
 
+        Author Tolstoy3 = Tolstoy;
         System.out.println();
 
         Author Esenin = new Author("Sergey", "Esenin");
         Book poems = new Book("Fucken Love", 1901,Esenin);
+        Book poems2 = new Book("Fucken Love", 1901,Esenin);
+
+        Book poems3=poems;
+
         poems.setYear(1902);
+        poems.setYear(1901);
+
         System.out.println(poems.getYear());
         System.out.println(poems.getTitle());
         Book warAndPeace = new Book("War and Peace", 1873,Tolstoy);
         System.out.println(Tolstoy.getName());
+        System.out.println(poems.toString());
+
+
+        System.out.println("Tolstoy==Tolstoy2 "+(Tolstoy==Tolstoy2));
+        System.out.println("Tolstoy==Tolstoy3 "+(Tolstoy==Tolstoy3));
+        System.out.println("Tolstoy.equals(Tolstoy2) "+(Tolstoy.equals(Tolstoy2)));
+        System.out.println("Tolstoy.equals(Tolstoy3) "+(Tolstoy.equals(Tolstoy3)));
+
+        System.out.println(Tolstoy.hashCode());
+        System.out.println(Tolstoy2.hashCode());
+        System.out.println(Tolstoy3.hashCode());
+
+
+        System.out.println("poems==poems2 "+(poems==poems2));
+        System.out.println("poems==poems3 "+(poems==poems3));
+        System.out.println("poems.equals(poems2)"+(poems.equals(poems2)));
+        System.out.println("poems.equals(poems3) "+(poems.equals(poems3)));
+
+        System.out.println(poems.hashCode());
+        System.out.println(poems2.hashCode());
+        System.out.println(poems3.hashCode());
+
+
 
     }
 }
